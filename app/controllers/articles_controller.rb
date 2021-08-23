544c653556e7a467cbbbcf0c551aef7782cf1563
LucasFerroHAILab <lucas.ferro.2000@hotmail.com>
2021-08-23 15:48:12 -0300

    Completed articles functions and links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,37 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require[:article].permti(:title, :description))
-    @article = Article.save
-    redirect_to article_path(@article)
-    #redirect_to @article --- can also be used
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to article_path(@article)
+      #redirect_to @article --- can also be used
+      flash[:notice] = "Article created successfully"
+    else
+      render 'new'
+    end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
