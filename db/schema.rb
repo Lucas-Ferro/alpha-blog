f8ecfb7b2eed4f0796961c4eecdb954024a04f13
LucasFerroHAILab <lucas.ferro.2000@hotmail.com>
2021-09-06 12:15:02 -0300

    create signup form within signup button

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_124211) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
